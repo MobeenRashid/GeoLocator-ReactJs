e5e19c4ce0c8c41173a9df67b9eb4eb2afa96af2
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-03 23:29:33 +0500

    Updated App.js to configure redux store provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import appRoutes from './routing/app.routes';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 
+const store = configureStore();
 class App extends Component {
+  componentDidMount() {
+    store.subscribe(() => { console.log(store.getState()); });
+  }
   render() {
     return (
-      <div className="App">
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            {appRoutes}
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
