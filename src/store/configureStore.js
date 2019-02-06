8de8fe2167a1f34064e369213f10c3b57a84804d
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-06 22:21:45 +0500

    Applied thunk middleware to redux store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+
 import appReducer from "../reducers/appReducer";
 
-const configureStore = (initialState) => createStore(appReducer, initialState);
+const configureStore = (initialState) => createStore(appReducer, initialState, applyMiddleware(thunk));
 export default configureStore;
