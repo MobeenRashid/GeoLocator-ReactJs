ddd318f9faca0ba5c23ad66b6acc41de1fa713ad
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-06 23:24:35 +0500

    App: added initial state to redux store

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import appRoutes from './routing/app.routes';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
+import initialState from './store/initialState';
 
-const store = configureStore();
+const store = configureStore(initialState);
 class App extends Component {
   componentDidMount() {
     store.subscribe(() => { console.log(store.getState()); });
