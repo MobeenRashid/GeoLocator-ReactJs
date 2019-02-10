8366a1be75e96f6770c1700884ea75af9f8fe4b3
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-10 14:27:23 +0500

    mockConstants renamed to __mockState

--- __mocks__/__mockState.js
+++ __mocks__/__mockState.js
@@ -1,20 +1,18 @@
-import { arrayToMap } from "../src/utils/arrayUtils";
 
 const __mockLocationList = [
     {
         id: '1',
         address: 'Berlin',
-        lng: "123",
-        lat: "456"
+        lng: '123',
+        lat: '456'
     },
     {
         id: '2',
         address: 'Munich',
-        lng: "124",
-        lat: "457"
+        lng: '124',
+        lat: '457'
     }
 ];
-
 
 const __mockState = {
     locations: __mockLocationList,
