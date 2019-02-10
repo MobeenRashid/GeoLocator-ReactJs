a7dd571865da2c65b4fab738a52f2893973fa3f4
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-10 18:22:49 +0500

    MapLocationArea: updated to container component

--- src/areas/MapLocationArea/MapLocationArea.js
+++ src/areas/MapLocationArea/MapLocationArea.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import locationAreaRoutes from './location.area.routes';
+import { connect } from 'react-redux';
+import { fetchLocationsIfRequired } from '../../actions/locationActions';
 
-var MapLocationArea = () => (<div className="map-location-area">{locationAreaRoutes}</div>)
-export default MapLocationArea;
+class MapLocationArea extends Component {
+
+    //#region Life Cycle Hooks
+    async componentDidMount() {
+        await this.props.onDidMount();
+    }
+    //#endregion
+
+    render() {
+        return (<div className="map-location-area">
+            {locationAreaRoutes}
+        </div>);
+    }
+}
+
+MapLocationArea.propTypes = {
+    onDidMount: PropTypes.func.isRequired
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onDidMount: async () => {
+            return dispatch(fetchLocationsIfRequired());
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(MapLocationArea);
