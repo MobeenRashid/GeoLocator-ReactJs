7f31f5df5aa428ff194d00da577c0231e7a4a3d8
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-10 18:26:10 +0500

    App.js: cleaned unneccessary code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import appRoutes from './routing/app.routes';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,21 +7,14 @@ import { Provider } from 'react-redux';
 import initialState from './store/initialState';
 
 const store = configureStore(initialState);
-class App extends Component {
-  componentDidMount() {
-    store.subscribe(() => { console.log(store.getState()); });
-  }
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            {appRoutes}
-          </div>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className="App">
+        {appRoutes}
+      </div>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
