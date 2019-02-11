f2f549037e355b07834f5204e9c0b1a3c762eb2e
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-11 22:03:14 +0500

    Implemented auth reducer and related components

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { locationReducer, requestingLocationReducer } from './location/locationReducer'
-
+import { authReducer } from './auth/authReducer'
 const appReducer = combineReducers({
     locations: locationReducer,
-    requestingLocations: requestingLocationReducer
+    requestingLocations: requestingLocationReducer,
+    googleMapKey: authReducer
 });
 
 export default appReducer;
