df7c0d0541bef116f22276d2bc8eaa3dc9fd61b0
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-11 22:10:55 +0500

    Refactored BasicMap changes to MapLocationArea component

--- src/areas/MapLocationArea/components/MapLocationContainer.js
+++ src/areas/MapLocationArea/components/MapLocationContainer.js
@@ -4,8 +4,6 @@ import { Row, Col } from 'antd';
 import BasicMap from '../../../components/GoogleMap/BasicMap';
 import LocationContainer from './LocationList/LocationContainer';
 
-const appSetting = require('../../../app.settings');
-
 class MapAndLocationContainer extends PureComponent {
     render() {
         let { locationData } = this.props;
@@ -13,14 +11,7 @@ class MapAndLocationContainer extends PureComponent {
             <div className="map-location-container" >
                 <Row>
                     <Col className="map-holder" lg={12} md={12} sm={24}>
-                        <BasicMap
-                            isMarkerShown
-                            locations={locationData}
-                            googleMapURL={appSetting.google_map_url}
-                            loadingElement={<div className="map-loading-div" style={{ height: '100%' }} />}
-                            containerElement={<div className="map-container" style={{ height: '100%' }} />}
-                            mapElement={<div className="map-element" style={{ height: '100%' }} />}
-                        ></BasicMap>
+                        <BasicMap locations={locationData} />
                     </Col>
                     <Col className="locations-holder" lg={12} md={12} sm={24}>
                         <LocationContainer {...this.props}></LocationContainer>
