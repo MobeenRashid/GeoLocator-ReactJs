35aa114142a9b6965abc1d9f1ade692a9c87751b
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-11 22:16:18 +0500

    Updated environment variable references in UrlBuilder

--- src/builder/UrlBuilder.spec.js
+++ src/builder/UrlBuilder.spec.js
@@ -1,21 +1,20 @@
 import UrlBuilder from "./UrlBuilder";
-import environment from "../common/environment";
 
 describe('UrlBuilder', () => {
     test('Should build paths segments correctly', () => {
-        let urlBuilder = new UrlBuilder(environment.remoteOrigin)
+        let urlBuilder = new UrlBuilder(process.env.REACT_APP_REMOTE_ORIGIN)
         urlBuilder.AddPathSegment('user')
             .AddPathSegment('get')
             .AddPathSegment(1);
-        expect(urlBuilder.Build()).toBe(`${environment.remoteOrigin}/user/get/1`);
+        expect(urlBuilder.Build()).toBe(`${process.env.REACT_APP_REMOTE_ORIGIN}/user/get/1`);
     });
 
     test('Should build query params correctly', () => {
-        let urlBuilder = new UrlBuilder(environment.remoteOrigin)
+        let urlBuilder = new UrlBuilder(process.env.REACT_APP_REMOTE_ORIGIN)
         urlBuilder.AddPathSegment('user')
             .AddQueryStringParam('id=1')
             .AddQueryStringParam('name=Mobeen')
             .AddPathSegment('get');
-        expect(urlBuilder.Build()).toBe(`${environment.remoteOrigin}/user/get?id=1&name=Mobeen`);
+        expect(urlBuilder.Build()).toBe(`${process.env.REACT_APP_REMOTE_ORIGIN}/user/get?id=1&name=Mobeen`);
     });
 });
