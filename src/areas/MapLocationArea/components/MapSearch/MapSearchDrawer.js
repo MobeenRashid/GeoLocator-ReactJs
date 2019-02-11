16e513ecfb746ed3886fd5b75dbba98b0f454483
Mobeen Rashid <mobeenrashed@outlook.com>
2019-02-11 22:15:00 +0500

    Connected MapSearchDrawer to redux store, improved implementation, updated tests

--- src/areas/MapLocationArea/components/MapSearch/MapSearchDrawer.js
+++ src/areas/MapLocationArea/components/MapSearch/MapSearchDrawer.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Drawer } from 'antd';
 import MapLocationSearch from './MapLocationSearch';
 import BasicMap from '../../../../components/GoogleMap/BasicMap';
 import DrawerFooter from './DrawerFooter';
-
-const appSetting = require('../../../../app.settings');
 
 const MapSearchDrawer = (props) => {
     return (
@@ -17,20 +16,22 @@ const MapSearchDrawer = (props) => {
             height={window.innerHeight}
         >
             <div className="drawer-content">
-                <MapLocationSearch disabled={props.disabled} reset={!props.visible} onLocationSelect={props.onLocationSelect} defaultLocation={props.selectedLocation}></MapLocationSearch>
-                <BasicMap
-                    isMarkerShown
-                    locations={props.locations}
-                    googleMapURL={appSetting.google_map_url}
-                    loadingElement={<div className="map-loading-div" style={{ height: '100%' }} />}
-                    containerElement={<div className="map-container" style={{ height: '100%' }} />}
-                    mapElement={<div className="map-element" style={{ height: '100%' }} />}
-                >
-                </BasicMap>
+                <MapLocationSearch
+                    disabled={props.disabled}
+                    reset={!props.visible}
+                    onLocationSelect={props.onLocationSelect}
+                    defaultLocation={props.selectedLocation} />
+                <BasicMap locations={props.locations} />
             </div>
             <DrawerFooter {...props}></DrawerFooter>
         </Drawer>
     );
 }
 
-export default MapSearchDrawer;
+function mapStateToProps(state) {
+    return {
+        mapKey: state.googleMapKey
+    }
+}
+
+export default connect(mapStateToProps)(MapSearchDrawer);
--- src/areas/MapLocationArea/components/MapSearch/MapSearchDrawer.spec.js
+++ src/areas/MapLocationArea/components/MapSearch/MapSearchDrawer.spec.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { BrowserRouter } from 'react-router-dom';
 import MapSearchDrawer from './MapSearchDrawer';
+import { Provider } from 'react-redux';
+import configureStore from '../../../../store/configureStore';
+import { __mockState } from '../../../../../__mocks__/__mockState';
 
+const store = configureStore(__mockState);
 describe("Map search drawer", () => {
     test("Should render correctly", () => {
-        const component = mount(<BrowserRouter>
-            <MapSearchDrawer></MapSearchDrawer>
-        </BrowserRouter>);
+        const component = mount(<Provider store={store}>
+            <BrowserRouter>
+                <MapSearchDrawer></MapSearchDrawer>
+            </BrowserRouter>
+        </Provider>);
         expect(component).toMatchSnapshot();
         component.unmount();
     });
